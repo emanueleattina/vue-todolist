14c2f816fc6b8009dd1d13369e3dc98d61566abb
emanuele <emattina97@gmail.com>
2021-03-25 17:49:18 +0100

    Carico esercizio parzialmente completo
    Bonus fatto, manca divisione in due array di task finiti e non finiti

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
 var app = new Vue({
     el: '#root',
     data: {
+        // done: '',
         inputTodo: '',
         todoList: [
             {
                 title: 'leggere',
-                fatto: ''
+                done: '',
+                modifica: 'nascondi'
+            },
+            {
+                title: 'mangiare',
+                done: 'finito',
+                modifica: 'nascondi'
+            },
+            {
+                title: 'dormire',
+                done: '',
+                modifica: 'nascondi'
             }
         ]
     },
@@ -14,14 +26,28 @@ var app = new Vue({
             if(this.inputTodo != '') {
                 let ogg = {
                     title: this.inputTodo,
-                    fatto: ''
+                    done: '',
+                    modifica: 'nascondi'
                 }
             this.todoList.push(ogg);
             this.inputTodo = '';
             }
         },
-        fatto: function() {
-            this.fatto = 'barrato';
+        fatto: function(todo) {
+            let i = this.todoList.indexOf(todo);
+            this.todoList[i].done = 'finito';
+        },
+        cancella: function(todo) {
+            let i = this.todoList.indexOf(todo);
+            this.todoList.splice(i, 1);
+        },
+        edit: function(todo) {
+            let i = this.todoList.indexOf(todo);
+            this.todoList[i].modifica = 'mostra';
+        },
+        endEdit: function(todo) {
+            let i = this.todoList.indexOf(todo);
+            this.todoList[i].modifica = 'nascondi';
         }
     }
 });
